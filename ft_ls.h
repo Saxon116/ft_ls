9f710988227116084ae34ae4fb35bc0e6c60ce1a
Nicolas KELLUM <nkellum@e2r8p19.42.fr>
2019-05-29 18:14:59 +0200

    finished getting file info with stat

--- ft_ls.h
+++ ft_ls.h
@@ -6,35 +6,40 @@
 /*   By: nkellum <nkellum@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 16:58:28 by nkellum           #+#    #+#             */
-/*   Updated: 2019/05/29 12:50:25 by nkellum          ###   ########.fr       */
+/*   Updated: 2019/05/29 17:51:29 by nkellum          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LS_H
 # define FT_LS_H
 
-# include <stdio.h>
-# include <dirent.h>
-# include <stdlib.h>
-# include "libft/libft.h"
+#include <stdio.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <uuid/uuid.h>
+#include <grp.h>
+#include <time.h>
+#include "libft/libft.h"
 
 typedef struct 	s_entry
 {
-	char		*name;
-	char		*rights;
-	int			hard_links;
-	char		*user;
-	char		*group;
-	int			size;
-	char		*date_month_modified;
-	int			date_day_modified;
-	char		*date_time_modified;
-	char		*date_month_created;
-	int			date_day_created;
-	char		*date_time_created;
-	char		*date_month_accessed;
-	int			date_day_accessed;
-	char		*date_time_accessed;
+	int				is_folder;
+	char			*name;
+	char			*rights;
+	int				hard_links;
+	char			*user;
+	char			*group;
+	int				size;
+	char			*date_month_modified;
+	int				date_day_modified;
+	char			*date_time_modified;
+	long			date_accessed;
+	struct s_entry	*next;
 }				t_entry;
 
 
--- main.c
+++ main.c
@@ -6,15 +6,11 @@
 /*   By: nkellum <nkellum@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/03 15:10:14 by nkellum           #+#    #+#             */
-/*   Updated: 2019/05/26 11:10:42 by nkellum          ###   ########.fr       */
+/*   Updated: 2019/05/29 18:13:39 by nkellum          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <dirent.h>
-#include <errno.h>
-#include <string.h>
-#include "libft/libft.h"
+#include "ft_ls.h"
 
 void list_dir(DIR *pDir)
 {
@@ -29,12 +25,74 @@ void list_dir(DIR *pDir)
 	closedir(pDir);
 }
 
+int get_day(char *date)
+{
+	int i;
+
+	i = 0;
+	while(!ft_isdigit(date[i]))
+		i++;
+	return (ft_atoi(date + i));
+}
+
+char *permissions(mode_t perm)
+{
+    char *modeval;
+
+	if((modeval = malloc(sizeof(char) * 9 + 1)) == NULL)
+		return 0;
+    modeval[0] = (perm & S_IRUSR) ? 'r' : '-';
+    modeval[1] = (perm & S_IWUSR) ? 'w' : '-';
+    modeval[2] = (perm & S_IXUSR) ? 'x' : '-';
+    modeval[3] = (perm & S_IRGRP) ? 'r' : '-';
+    modeval[4] = (perm & S_IWGRP) ? 'w' : '-';
+    modeval[5] = (perm & S_IXGRP) ? 'x' : '-';
+    modeval[6] = (perm & S_IROTH) ? 'r' : '-';
+    modeval[7] = (perm & S_IWOTH) ? 'w' : '-';
+    modeval[8] = (perm & S_IXOTH) ? 'x' : '-';
+    modeval[9] = '\0';
+    return modeval;
+}
+
+
+ t_entry *add_new_entry(char *path, char *entry_name, int is_folder)
+{
+	t_entry	*entry;
+	struct stat	*pstat;
+
+	if((pstat = malloc(sizeof(struct stat))) == NULL)
+		return 0;
+	if((entry = malloc(sizeof(t_entry))) == NULL)
+		return 0;
+	stat(path, pstat);
+	entry->is_folder = is_folder;
+	entry->name = ft_strdup(entry_name);
+	entry->rights = permissions(pstat->st_mode);
+	entry->hard_links = pstat->st_nlink;
+	entry->size = pstat->st_size;
+	entry->user = ft_strdup(getpwuid(pstat->st_uid)->pw_name);
+	entry->group = ft_strdup(getgrgid(pstat->st_gid)->gr_name);
+	entry->date_day_modified = get_day(ctime(&pstat->st_mtimespec.tv_sec));
+	entry->date_month_modified =
+	ft_strsub(ctime(&pstat->st_mtimespec.tv_sec), 3, 4);
+	entry->date_time_modified =
+	ft_strsub(ctime(&pstat->st_mtimespec.tv_sec), 11, 5);
+	entry->date_accessed = pstat->st_mtimespec.tv_sec;
+	entry->next = NULL;
+	return entry;
+}
+
 int list_dir_recursive(char *dirname)
 {
-	struct dirent *pDirent;
+	struct dirent	*pDirent;
+
+ 	t_entry	*list_start;
+	t_entry	*list_current;
 	DIR *pDir;
 	char path[ft_strlen(dirname) + 255]; // 255 more chars for subdirectory path
 
+	list_current = NULL;
+	ft_strcpy(path, dirname); // set path to the current directory path
 	pDir = opendir(dirname);
 	if (pDir == NULL)
 	{
@@ -44,13 +102,45 @@ int list_dir_recursive(char *dirname)
 	printf("%s:\n", dirname);
 	while ((pDirent = readdir(pDir)) != NULL)
 	{
-		if(pDirent->d_name[0] != '.') // ignore hidden entries
-			printf("%s ", pDirent->d_name); // list entries normally
+		if(pDirent->d_name[0] != '.')
+		{ // ignore hidden entries
+			//printf("%s ", pDirent->d_name); // list entries normally
+		if(dirname[ft_strlen(dirname) - 1] != '/')
+			ft_strcat(path, "/");
+		ft_strcat(path, pDirent->d_name);
+		if(!list_current)
+		{
+			list_current = add_new_entry(path, pDirent->d_name,
+				pDirent->d_type == DT_DIR);
+			list_start = list_current;
+		}
+		else
+		{
+			list_current->next = add_new_entry(path, pDirent->d_name,
+				pDirent->d_type == DT_DIR);
+			list_current = list_current->next;
+		}
+		//printf("user is %s ", add_new_entry(path, pDirent->d_name)->user);
+		ft_bzero(path + ft_strlen(dirname),
+		ft_strlen(pDirent->d_name) +
+		dirname[ft_strlen(dirname) - 1] != '/');
+	}
+	}
+	list_current = list_start;
+	while(list_current)
+	{
+		printf("%c%s %d %s %s %d %s %d %s %s\n",
+		list_current->is_folder ? 'd' : '-', list_current->rights,
+		list_current->hard_links, list_current->user, list_current->group,
+		list_current->size, list_current->date_month_modified,
+		list_current->date_day_modified, list_current->date_time_modified,
+		list_current->name);
+		list_current = list_current->next;
 	}
 	printf("\n");
 	closedir(pDir);
 	pDir = opendir(dirname); // resetting pDir to first file entry for new loop
-	ft_strcpy(path, dirname); // set path to the current directory path
+
 	while ((pDirent = readdir(pDir)) != NULL)
 	{
 		if(pDirent->d_name[0] != '.')
