1ed7292ec03d12b615f7f7e3a7f20dec787bba24
Jeremie MONDINO <m.ondino@hotmail.fr>
2019-07-02 15:43:22 +0200

    norme

--- Makefile
+++ Makefile
@@ -6,18 +6,15 @@
 #    By: jmondino <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/07/02 14:59:03 by jmondino          #+#    #+#              #
-#    Updated: 2019/07/02 15:01:24 by jmondino         ###   ########.fr        #
+#    Updated: 2019/07/02 15:43:01 by jmondino         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
-
 
 NAME = ft_ls
 
 SRC = src/acl.c src/add_new_entry.c src/column.c src/column_aux.c src/display.c src/fill_list.c src/free.c src/ft_ls.h src/libft.h src/main.c src/offsets.c src/parsing.c src/parsing_aux.c src/print_color.c src/print_l.c src/print_l_aux.c src/recursive.c src/sort_list.c src/sort_list_aux.c src/sort_tab.c src/xattr.c
 
-OBJ = $(SRC:.c=.o)
-
-FLAGS = -Wextra -Wall -Werror
+FLAGS = -Wextra -Wall -Werror -g
 
 ifndef VERBOSE
 .SILENT:
@@ -25,17 +22,18 @@ endif
 
 all:$(NAME)
 
-$(NAME): $(OBJ)
-	gcc $(FLAGS) -o $(NAME) $(OBJ)
+$(NAME): $(SRC)
+	make -C libft
+	gcc $(FLAGS) -o $(NAME) $(SRC) libft/libft.a
 	echo "\033[32m$(NAME) compiled.\033[0m"
 
 clean:
-	/bin/rm -f $(OBJ)
-	echo "\033[32mAll .o have been erased.\033[0m"
+	make -C libft clean
+	echo "\033[32mObjects files deleted.\033[0m"
 
 fclean: clean
-	/bin/rm -f $(NAME)
-	echo "\033[32m$(NAME) erased.\033[0m"
+	make -C libft fclean
+	rm -f ft_ls
 
 re: fclean all
 
--- recursive.c
+++ recursive.c
@@ -12,87 +12,87 @@
 
 #include "ft_ls.h"
 
-void	ft_print_dir_name(t_entry *list_start, t_args *pargs, char *dirname)
+void	ft_print_dir_name(t_entry *list_start, t_shit *pShit, char *dirname)
 {
-	if (!(ft_iscinstr(pargs->flags, 'R')))
+	if (!(ft_iscinstr(pShit->flags, 'R')))
 	{
-		if (ft_strcmp(dirname, "./") && (pargs->dirs[1] || pargs->files[0]
-										 || pargs->error != 0))
+		if (ft_strcmp(dirname, "./") && (pShit->dirs[1] || pShit->files[0]
+										 || pShit->error != 0))
 			printf(RESET"%s:\n", dirname);
 	}
 	else
-		if (pargs->subdir != 0 || pargs->dirs[1] || pargs->files[0]
-			|| pargs->error != 0)
+		if (pShit->subdir != 0 || pShit->dirs[1] || pShit->files[0]
+			|| pShit->error != 0)
 			printf(RESET"%s:\n", dirname);
 	ft_print_column(list_start);
 }
 
-void    list_dir_recursive(char *dirname, char *name, t_args *pargs)
+void    list_dir_recursive(char *dirname, char *name, t_shit *pShit)
 {
-    struct dirent *pdirent;
+    struct dirent *pDirent;
     t_entry *list_start;
 	t_entry *list_current;
-    DIR *pdir;
+    DIR *pDir;
     char path[ft_strlen(dirname) + 255];
 
-    pdirent = NULL;
+    pDirent = NULL;
     list_start = NULL;
     ft_strcpy(path, dirname);
-    pdir = opendir(dirname);
-    if (pdir == NULL)
+    pDir = opendir(dirname);
+    if (pDir == NULL)
 	{
-        if (pargs->subdir != 0 || pargs->dirs[1] || pargs->files[0]
-            || pargs->error != 0)
+        if (pShit->subdir != 0 || pShit->dirs[1] || pShit->files[0]
+            || pShit->error != 0)
             printf("%s:\n", path);
         printf(RESET"ft_ls: %s: Permission denied\n", name);
         return ;
     }
-    if (ft_iscinstr(pargs->flags, 'a') || ft_iscinstr(pargs->flags, 'f'))
-        list_start = fill_list_a(pdir, pargs, path, dirname);
+    if (ft_iscinstr(pShit->flags, 'a') || ft_iscinstr(pShit->flags, 'f'))
+        list_start = fill_list_a(pDir, pShit, path, dirname);
     else
-        list_start = fill_list(pdir, pargs, path, dirname);
-    if (!ft_iscinstr(pargs->flags, 'f'))
+        list_start = fill_list(pDir, pShit, path, dirname);
+    if (!ft_iscinstr(pShit->flags, 'f'))
 	{
-        list_start = ft_tri_ascii(list_start, pargs);
-		if (ft_iscinstr(pargs->flags, 't'))
+        list_start = ft_tri_ascii(list_start, pShit);
+		if (ft_iscinstr(pShit->flags, 't'))
 		{
-			if (ft_iscinstr(pargs->flags, 'u'))
-				list_start = ft_tri_access(list_start, pargs);
+			if (ft_iscinstr(pShit->flags, 'u'))
+				list_start = ft_tri_access(list_start, pShit);
 			else
-				list_start = ft_tri_date(list_start, pargs);
+				list_start = ft_tri_date(list_start, pShit);
 		}
 	}
-    if (ft_iscinstr(pargs->flags, 'l') || ft_iscinstr(pargs->flags, 'g'))
-        display_entries_l(list_start, pargs, dirname);
+    if (ft_iscinstr(pShit->flags, 'l') || ft_iscinstr(pShit->flags, 'g'))
+        display_entries_l(list_start, pShit, dirname);
     else
-        ft_print_dir_name(list_start, pargs, dirname);
+        ft_print_dir_name(list_start, pShit, dirname);
     lstdel(&list_start);
-    closedir(pdir);
-    if (ft_iscinstr(pargs->flags, 'R'))
+    closedir(pDir);
+    if (ft_iscinstr(pShit->flags, 'R'))
     {
-        pdir = opendir(dirname);
-        list_start = fill_list_rdir(pdir, pargs, path, dirname);
-		closedir(pdir);
+        pDir = opendir(dirname);
+        list_start = fill_list_rdir(pDir, pShit, path, dirname);
+		closedir(pDir);
 		if(list_start == NULL)
 			return ;
-		if (!ft_iscinstr(pargs->flags, 'f'))
-			list_start = ft_tri_ascii(list_start, pargs);
-		if (ft_iscinstr(pargs->flags, 't'))
+		if (!ft_iscinstr(pShit->flags, 'f'))
+			list_start = ft_tri_ascii(list_start, pShit);
+		if (ft_iscinstr(pShit->flags, 't'))
 		{
-			if (ft_iscinstr(pargs->flags, 'u'))
-				list_start = ft_tri_access(list_start, pargs);
+			if (ft_iscinstr(pShit->flags, 'u'))
+				list_start = ft_tri_access(list_start, pShit);
 			else
-				list_start = ft_tri_date(list_start, pargs);
+				list_start = ft_tri_date(list_start, pShit);
 		}
 		list_current = list_start;
 		while(list_current)
 		{
-			pargs->subdir++;
+			pShit->subdir++;
             printf("\n");
             if (dirname[ft_strlen(dirname) - 1] != '/')
                 ft_strcat(path, "/");
             ft_strcat(path, list_current->name);
-            list_dir_recursive(path, list_current->name, pargs);
+            list_dir_recursive(path, list_current->name, pShit);
             ft_bzero(path + ft_strlen(dirname),
                      ft_strlen(list_current->name));
 			list_current = list_current->next;
